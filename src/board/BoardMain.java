dd5e1a1f6be60e0ffdb8d1854c655ca9516f110e
clclhive <jwk728@naver.com>
2025-11-24 15:02:36 +0900

    test for git

--- src/board/BoardMain.java
+++ src/board/BoardMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BoardMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub test to github
 		Scanner scan = new Scanner(System.in);
 		BoardInterface manager = new BoardController();
 		
